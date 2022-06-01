5943c52f2953c93a9bd605a6f4bcbffe89e1d7a6
Archariya Pliansaneh <apliansaneh@mybenefitexpress.com>
2022-06-01 17:10:08 -0500

    refactors

--- rgbToHex.js
+++ rgbToHex.js
@@ -15,11 +15,11 @@ const _opacity_85 = 0.85;
 
 /*----------------------test-------------------------- */
 let rgb = [31, 74, 121]; // #1F4A79 - 31, 74,121
+passthroughOver('.box-50', '--color-name-50', mixer(_backgroundOn.Black, rgb, 0));
 passthroughOver('.box-40', '--color-name-40', mixer(_backgroundOn.White, rgb, _opacity_20));
 passthroughOver('.box-30', '--color-name-30', mixer(_backgroundOn.White, rgb, _opacity_35));
 passthroughOver('.box-20', '--color-name-20', mixer(_backgroundOn.White, rgb, _opacity_65));
 passthroughOver('.box-10', '--color-name-10', mixer(_backgroundOn.White, rgb, _opacity_85));
-passthroughOver('.box-50', '--color-name-50', mixer(_backgroundOn.Black, rgb, 0));
 passthroughOver('.box-60', '--color-name-60', mixer(_backgroundOn.Black, rgb, _opacity_35));
 passthroughOver('.box-70', '--color-name-70', mixer(_backgroundOn.Black, rgb, _opacity_65));
 passthroughOver('.box-80', '--color-name-80', mixer(_backgroundOn.Black, rgb, _opacity_85));
@@ -27,20 +27,20 @@ passthroughOver('.box-80', '--color-name-80', mixer(_backgroundOn.Black, rgb, _o
 /* attach events */
 document.getElementById('colorMe').addEventListener('input', (e) => {
     _input = document.getElementById('colorMe').value;
-    let rgb = convertHexToRGB(_input); 
+    let rgb = HexToRGB(_input); 
 
     //----------------- display colors
+    passthroughOver('.box-50', '--color-name-50', mixer(_backgroundOn.Black, rgb, 0));
     passthroughOver('.box-40', '--color-name-40', mixer(_backgroundOn.White, rgb, _opacity_20));
     passthroughOver('.box-30', '--color-name-30', mixer(_backgroundOn.White, rgb, _opacity_35));
     passthroughOver('.box-20', '--color-name-20', mixer(_backgroundOn.White, rgb, _opacity_65));
     passthroughOver('.box-10', '--color-name-10', mixer(_backgroundOn.White, rgb, _opacity_85));
-    passthroughOver('.box-50', '--color-name-50', mixer(_backgroundOn.Black, rgb, 0));
     passthroughOver('.box-60', '--color-name-60', mixer(_backgroundOn.Black, rgb, _opacity_35));
     passthroughOver('.box-70', '--color-name-70', mixer(_backgroundOn.Black, rgb, _opacity_65));
     passthroughOver('.box-80', '--color-name-80', mixer(_backgroundOn.Black, rgb, _opacity_85));
 });
 
-function convertHexToRGB(input) {
+function HexToRGB(input) {
     let switch_cnt = 1;
     let total = 0;
     let index = 0;
@@ -85,7 +85,7 @@ function blend(overlay, background, opacity) {
 
 function passthroughOver(box, background, color) {
     const boxSelector = document.querySelector(box);
-    let p = document.querySelector(`${box} div.title p`);
+    let p = document.querySelector(`${box} p`);
     p.innerHTML = color;
     boxSelector.style.setProperty(background, color);
 }
